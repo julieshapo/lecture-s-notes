cf36628f080c09659c3aa56c41ef2f0d0e2f945f
Julie Shapovalenko <jools.shapo@gmail.com>
2022-12-09 13:31:12 +0200

    add calendar

--- js/select.js
+++ js/select.js
@@ -15,7 +15,7 @@ for (i = 0; i < 1; i++) {
   /* For each element, create a new DIV that will contain the option list: */
   b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
-  for (j = 0; j < 4; j++) {
+  for (j = 0; j < 6; j++) {
     /* For each option in the original select element,
     create a new DIV that will act as an option item: */
     c = document.createElement("DIV");
